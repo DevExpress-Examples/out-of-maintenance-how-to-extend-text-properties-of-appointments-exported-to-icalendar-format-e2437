71379793b4182d9c4b5167df7586fd1f66ef1c59
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+#region #usings
+using DevExpress.XtraScheduler.iCalendar;
+using DevExpress.XtraScheduler;
+using DevExpress.XtraScheduler.iCalendar.Components;
+#endregion #usings
+
+namespace iCalendarDemo {
+    public partial class Form1 : Form {
+        private const string RecipientsDataColumn = "RecipientsData";
+
+        public Form1() {
+            InitializeComponent();
+            schedulerControl1.Start = new DateTime(2010, 09, 20);
+
+            //this.eventsTableAdapter.Adapter.RowUpdated += new OleDbRowUpdatedEventHandler(Adapter_RowUpdated);
+        }
+
+        void Adapter_RowUpdated(object sender, OleDbRowUpdatedEventArgs e) {
+            if (e.Status == UpdateStatus.Continue && e.StatementType == StatementType.Insert) {
+                int id = 0;
+                using (OleDbCommand cmd = new OleDbCommand("SELECT @@IDENTITY", this.eventsTableAdapter.Connection)) {
+                    id = (int)cmd.ExecuteScalar();
+                }
+                e.Row["UID"] = id;
+            }
+        }
+        
+        private void schedulerStorage1_AppointmentsInserted(object sender, DevExpress.XtraScheduler.PersistentObjectsEventArgs e) {
+            this.eventsTableAdapter.Update(this.dsEvents);
+            this.dsEvents.AcceptChanges();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // TODO: This line of code loads data into the 'dsEvents.Events' table. You can move, or remove it, as needed.
+            this.eventsTableAdapter.Fill(this.dsEvents.Events);
+
+        }
+
+        private void schedulerControl1_InitAppointmentDisplayText(object sender, DevExpress.XtraScheduler.AppointmentDisplayTextEventArgs e) {
+            string attendees = "\r\nRequired Attendees: " + Convert.ToString(e.Appointment.CustomFields[RecipientsDataColumn]); 
+            e.Description = e.Description + attendees;
+        }
+        private void schedulerControl1_InitNewAppointment(object sender, AppointmentEventArgs e) {
+            e.Appointment.CustomFields[RecipientsDataColumn] = "test@company.com";
+        }
+
+        private void schedulerControl1_EditAppointmentFormShowing(object sender, AppointmentFormEventArgs e) {
+            using (CustomAppointmentEditForm f = new CustomAppointmentEditForm(schedulerControl1, e.Appointment)) {
+                f.LookAndFeel.ParentLookAndFeel = schedulerControl1.LookAndFeel.ParentLookAndFeel;
+                e.DialogResult = f.ShowDialog();
+                e.Handled = true;
+            }
+
+        }
+
+        private void btnExport_Click(object sender, EventArgs e) {
+            using(SaveFileDialog fileDialog = new SaveFileDialog()) {
+                fileDialog.Filter = "iCalendar files (*.ics)|*.ics";
+                fileDialog.FilterIndex = 1;
+                fileDialog.FileName = "SchedulerEvents";
+
+                if (fileDialog.ShowDialog() != DialogResult.OK)
+                    return;
+                try {
+                    using (Stream stream = fileDialog.OpenFile())
+                        ExportAppointments(stream);
+                } catch {
+                    MessageBox.Show("Error: could not export appointments");
+                }
+            }
+        }
+        #region #export
+        void ExportAppointments(Stream stream) {
+            if (stream == null)
+                return;
+            iCalendarExporter exporter = new iCalendarExporter(schedulerStorage1);
+            exporter.AppointmentExporting += 
+                new AppointmentExportingEventHandler(exporter_AppointmentExporting);
+            exporter.Export(stream);
+        }
+        void exporter_AppointmentExporting(object sender, AppointmentExportingEventArgs e) {
+            string s = Convert.ToString(e.Appointment.CustomFields[RecipientsDataColumn]);
+            string[] attendees = s.Split(';');
+
+            iCalendarAppointmentExportingEventArgs args = e as iCalendarAppointmentExportingEventArgs;
+            AddEventAttendees(args.VEvent, attendees);
+        }
+        private void AddEventAttendees(VEvent ev, string[] addresses) {
+            int count = addresses.Length;
+            for (int i = 0; i < count; i++)
+                AddEventAttendee(ev, addresses[i]);
+        }
+        private void AddEventAttendee(VEvent ev, string address) {
+            TextProperty p = new TextProperty("ATTENDEE", 
+                String.Format("mailto:{0}", address));
+            p.AddParameter("CN", address);
+            p.AddParameter("RSVP", "TRUE");
+            ev.CustomProperties.Add(p);
+        }
+        #endregion #export
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to extend text properties of appointments exported to iCalendar format
+
+
+<p>This example demonstrates how you can export e-mail addresses stored in a database to the <strong>ics</strong> file as required attendees. </p><p>The XtraScheduler originally does not support meeting requests with attendees. To accomplish this task, use the CustomFields mechanism to bind e-mail data to scheduler appointments. To control the export process that is performed via the <strong>Export</strong> method of the <a href="http://documentation.devexpress.com/#WindowsForms/clsDevExpressXtraScheduleriCalendariCalendarExportertopic"> iCalendarExporter</a> class, handle its <a href="http://documentation.devexpress.com/#CoreLibraries/DevExpressXtraSchedulerExchangeAppointmentExporter_AppointmentExportingtopic">AppointmentExporting</a> event. Within the event handler, you can modify VEvent object's properties and extend it with custom text properties. <br />
+To create a new text property -<strong> ATTENDEE</strong> in this case - create an object of the <strong>DevExpress.XtraScheduler.iCalendar.Components.TextProperty</strong> type, specify required parameters (<strong>CN</strong> and <strong>RSVP</strong>) and add this property to the<strong> DevExpress.XtraScheduler.iCalendar.Components.iCalendarPropertyCollection</strong> of custom properties. This collection is accessible via the <strong>CustomProperties</strong> property of the current <strong>VEvent</strong> object.</p><p>The sample project contains an MS Access (mdb) database and a custom AppointmentForm so you can edit custom fields containing e-mail data. </p><p>To see the result, open the saved <strong>ics</strong> file in MS Outlook, double-click the imported appointment  and see addresses of the attendees displayed in the Meeting form.</p>
+
+<br/>
+
+
